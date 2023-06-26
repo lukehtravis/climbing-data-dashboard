4e1704402362bd310e2aba65b1ae7283d8560cbc
lukehtravis <lukehtravis@gmail.com>
2023-06-26 12:08:00 -0700

    remove unused files. Add some comments

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Form from './components/Form'
+
+function Hi() {
+  return (
+    <div>
+      <p>Choose A CSV file then upload it!</p>
+      <Form />
+    </div>
+  )
+}
+   
+export default Hi
