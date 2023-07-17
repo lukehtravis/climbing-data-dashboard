3e00c70195c548a22438a93934b0a2c43d0a7da7
lukehtravis <lukehtravis@gmail.com>
2023-07-17 11:31:45 -0700

    fixes broken data bug. adds documentation

--- app/dashboard/components/charts/MaxGrade.tsx
+++ app/dashboard/components/charts/MaxGrade.tsx
@@ -20,11 +20,12 @@ const MaxGradeChart: React.FC<Props> = ({data}: Props) => {
   const height: number = 500
   const datesByMonth = dateProcessor(data)
   
-  // Convenience function to pass into xScale...We could get this form datesByMonth with some more work
+  // Convenience function to pass into xScale...We could get this form datesByMonth with some more work inside the function but this makes it look simpler below
   const dates:(Date)[] = data
     .map(row => d3.timeParse("%Y-%m-%d")(row.Date))
     .filter((date): date is Date => date !== null);
 
+  // This takes our input data and organizes it in a way that will allow us to pass it into the d3.line() function
   const chartArray:LineData[] = datesByMonth.map(monthGroup => {
     let maxNumber = 0
     monthGroup.dates.forEach(row => {
@@ -32,35 +33,48 @@ const MaxGradeChart: React.FC<Props> = ({data}: Props) => {
         maxNumber = row["Converted Grade"]
       }
     })
-    return {month: new Date(`${monthGroup.year}-${monthGroup.month}`), grade: maxNumber}
-  })
+    return {
+      month: new Date(`${monthGroup.year}-${monthGroup.month}`), 
+      grade: maxNumber
+    }   
+    // After returning all these items, we use .filter to make sure that if any of the dates were messed up, we omit those because they will break the chart
+  }).filter(groupedItem => groupedItem.month.getMonth())
   
   useEffect(() => {
     if (data.length === 0) return;
+    
+    // Sets margins
     const margin = {top: 10, right: 30, bottom: 30, left: 60}
+
+    // Does some funky react shit to grab the svg element and work with it from within the react component lifecycle
     const svg = d3.select(svgRef.current);
+
+    // Designs the canvas and the axis locations
     svg
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-    //   .attr("transform",
-    //     "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // This thing takes in Date objects and converts them to x coordinates on our svg canvas
     const xScale = d3
       .scaleTime()
       .domain(d3.extent(dates) as unknown as [Date, Date])
       .range([0, width]);
   
+    // This thing takes in numbers representing grades and converts them to y coordinates on our svg canvas
     const yScale = d3
       .scaleLinear()
       .domain([0,34])
       .range([height,0]);
 
+    // Places the x axis
     svg.append("g")
       .attr("class", "x-axis")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(xScale));
     
+    // Places the y axis
     svg.append("g")
       .attr("class", "y-axis")
       .attr("transform", "translate("+ width + ", 0)")
