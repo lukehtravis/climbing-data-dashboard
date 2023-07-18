3b83f03fc75fcd91af0810fe86963e58ef3e98ec
sharanya204 <sharanya204@gmail.com>
2023-07-18 10:06:29 -0700

    committing to pull

--- app/dashboard/components/OnsightPercentage.tsx
+++ app/dashboard/components/OnsightPercentage.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import RawDataRow, { RawDataList } from "../types/raw-data-from-mountain-project";
 import * as d3 from 'd3';
+import { useEffect, useRef } from "react";
 
 // We can define what this object will look like after we decide exactly what we want to pass in
 interface Props {
@@ -16,6 +17,31 @@ const OnsightPercentage: React.FC<Props> = ({ data }: Props) => {
   const onsightSport = justSport.filter((oneRoute: RawDataRow) => onsightLeadStyle.includes(oneRoute["Lead Style"]));
   const groupAllSportByGrade = d3.group(justSport, (oneRoute: RawDataRow) => oneRoute.Rating);
   console.log(groupAllSportByGrade);
+  const margin = ({top: 20, right: 0, bottom: 30, left: 40}), width = 460 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
+  const x = d3.scaleBand().domain(justSport.map((oneRoute: RawDataRow) => oneRoute.Rating)).range([margin.left, width - margin.right]).padding(0.1);
+  const xAxis = d3.axisBottom(x).tickSizeOuter(0);
+  const y = d3.scaleLinear().domain(justSport.map((oneRoute: RawDataRow) => oneRoute.Rating)).nice().range([height - margin.bottom, margin.top]);
+  const yAxis = d3.axisLeft(y).ticks(null, "%")
+    
+    
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", `max-width: ${width}px; height: auto; font: 10px sans-serif; overflow: visible;`);
+
+  svg.append("g")
+    .attr("fill", "steelblue")
+    .selectAll("rect")
+    .data(justSport);
+    // .join("rect").attr("x", (d: string) => x(d.Rating)).attr("y", d => y(d.Rating)).attr("height", d => y(0) - y(d.Rating)).attr("width", x.bandwidth());
+
+  svg.append("g")
+    .call(xAxis);
+
+  svg.append("g")
+    .call(yAxis);
+  svg.node();
   // In this file, we take in some of the processed data, and pass it into a series of vis charts we can create independently and import in here
   return (
     <div>
@@ -23,7 +49,9 @@ const OnsightPercentage: React.FC<Props> = ({ data }: Props) => {
       {/* 
     group_all_sport_by_grade = just_sport.Rating.value_counts().rename_axis('Sport').copy()
     group_onsight_sport_by_grade = onsight_sport.Rating.value_counts().rename_axis('Onsight').copy() # count # of climbs for each grade */}
-      <p>{JSON.stringify(groupAllSportByGrade)}</p>
+      <p>
+        {JSON.stringify(groupAllSportByGrade)}
+      </p>
     </div>
 
   );
