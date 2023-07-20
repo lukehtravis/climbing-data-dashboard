59295993eb99d9239d8b62b589fbda81fc59ee7a
lukehtravis <lukehtravis@gmail.com>
2023-07-20 13:38:46 -0700

    add documentation and cleanup for dropdown form

--- app/dashboard/components/form-inputs/Dropdown.tsx
+++ app/dashboard/components/form-inputs/Dropdown.tsx
@@ -5,14 +5,20 @@ interface SelectProps {
   onChange: (selectedValue: string) => void; // Callback function when the selected value changes
 }
 
+// This input is a dropdown form element which will take an array of strings and a setState function and will allow users t
+// modify change the data source for the charts they are looking at. The options prop is an array of strings which will 
+// will need to correspond exactly to values in the data 
+// i.e. ["Sport", "Trad"] for the "Route Type" column, ["Onsight", "Flash", "Redpoint", "(etc..)"] for the "Lead Style" column) 
+
 const Dropdown: React.FC<SelectProps> = ({ options, onChange }) => {
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = event.target.value;
     onChange(selectedValue);
   };
 
+  if (!options) return null;
   return (
-    <select onChange={handleChange} defaultValue={"All"}>
+    <select onChange={handleChange} defaultValue={options[0]}>
       {options && options.map((option, index) => (
         <option key={index} value={option}>
           {option}
